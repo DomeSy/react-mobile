6d01d636bc9ed86d1e14166f7e38da55b9295d8f
Domesy <397163123@qq.com>
2021-01-08 15:09:56 +0800

    基本配置

--- config-overrides.js
+++ config-overrides.js
@@ -39,6 +39,7 @@ module.exports = override(
     }
   }),
   addWebpackAlias({
+    ["@mock"]: path.resolve(__dirname, "src/mock"),
     ["@components"]: path.resolve(__dirname, "src/components"),
     ["@pages"]: path.resolve(__dirname, "src/pages"),
     ["@assets"]: path.resolve(__dirname, "src/assets"),
