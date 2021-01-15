66cce5433af9d129748a8b53c4276197a539e0f3
Domesy <397163123@qq.com>
2021-01-15 13:42:26 +0800

    小图标

---
+++ src/components/Icon/Circular/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Base from '@images/icon/base.png'
+
+import './index.less'
+
+/**
+ * @module 小图标
+ * 
+ * src 接收的图片地址，
+ * block: 形状方形，默认圆形
+ */
+
+function index({src = Base, block = false}) {
+  return (
+    <div className={block ? "Circular" : "Circular Circular-Active"}>
+      <img src={src} alt=""/>
+    </div>
+  )
+}
+
+export default index
---
+++ src/components/Icon/index.js
@@ -0,0 +1,1 @@
+export { default as Circular } from './Circular';
