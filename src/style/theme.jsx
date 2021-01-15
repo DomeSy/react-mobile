a17f9edb71b5018be562c3a9f5de74e019a90881
Domesy <397163123@qq.com>
2021-01-15 11:47:11 +0800

    主题颜色

--- src/style/theme.jsx
+++ src/style/theme.jsx
@@ -1,6 +1,31 @@
 // 颜色
+/**
+ * @module 控制颜色
+ *  
+ * 主色：
+ *  #78A4FA(浅蓝色) #78A4FA(品牌栏) #346FC2(深蓝色)
+ * 
+ * 辅助色：
+ *  #78A4FA(蓝色) #13CE66(绿色) #FF4949(红色)  #FFC82C(黄色)
+ * 
+ * 中性色
+ *  #333333(黑色) #E5E5E5(灰色) #CCCCCC(灰色1)
+ * 
+ * 背景色
+ *  #ECF5FD(浅青色) #C5D9E8(深青色)
+ */
+
+
 const theme = {
-  '@red': "red"
+  '@blue': '#78A4FA',
+  '@blue1': '#78A4FA',
+  '@blue2': '#346FC2',
+  '@info': "#78A4FA",
+  '@green': '#13CE66',
+  '@red': '#FF4949',
+  '@yellow': '#FFC82C',
+  '@color': '#333333',
+  '@top': '#CCCCCC',
 }
 
 module.exports = theme
