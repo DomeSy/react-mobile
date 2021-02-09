70cfd7be9a129da824f07c164b0c8969dfc54893
Domesy <397163123@qq.com>
2021-02-09 15:58:37 +0800

    跳转逻辑

--- src/unilts/jump/index.jsx
+++ src/unilts/jump/index.jsx
@@ -8,6 +8,7 @@ import { Control } from 'react-keeper';
  * @param replace 重定向（与go一样）
  * @param get 获取地址栏参数
  * @param goSrc 跳转外部地址 
+ * @param title 设置标题
  * 
  * @param url 跳转的地址
  * @param params 跳转时所带的参数
@@ -54,6 +55,11 @@ class Jump{
       }
     }
     window.location.href = str.length === 0 ? url : `${url}?${str.substring(0, str.length-1)}`
+  }
+
+  static title = (title = '') => {
+    document.title = title;
+    return;
   }
 } 
 export default Jump
