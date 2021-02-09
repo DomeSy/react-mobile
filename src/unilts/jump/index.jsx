21906a12b02c90a5e663e6690127712457e190d3
Domesy <397163123@qq.com>
2021-02-09 12:12:13 +0800

    跳转封装

--- src/unilts/jump/index.jsx
+++ src/unilts/jump/index.jsx
@@ -3,7 +3,7 @@ import { Control } from 'react-keeper';
 /**
  * @module 跳转逻辑封装（并获取参数）
  * 
- * @param go 跳转，记录地址栏，一个参数可传字符串(默认跳转首页)
+ * @param go 跳转，记录地址栏，一个参数可传字符串(默认跳转首页),没有参数可以直接跳转
  * @param back 回退(默认回退1)
  * @param replace 重定向（与go一样）
  * @param get 获取地址栏参数
@@ -11,7 +11,9 @@ import { Control } from 'react-keeper';
  */
 
 class Jump{
-  static go = ({url='/', params={}}) => {
+  static go = (data) => {
+    let { url='/', params={} } = data;
+    if(typeof data === 'string') url = data;
     if(typeof params === 'string'){
       Control.go(url, {params})
       return;
@@ -25,8 +27,10 @@ class Jump{
     Control.go(Number(back))
   }
 
-  static replace = ({url='/', params={}}) => {
-  if(typeof params === 'string'){
+  static replace = (data) => {
+    let { url='/', params={} } = data;
+    if(typeof data === 'string') url = data;
+    if(typeof params === 'string'){
       Control.replace(url, {params})
       return;
     }else{
