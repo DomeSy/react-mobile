f86b2b2592ed909896b28b891e252a32e74abe66
Domesy <397163123@qq.com>
2021-02-17 23:55:32 +0800

    解决键盘弹起后遮挡输入框的问题

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,15 @@ class Home extends Component {
     const url = 'http://www.domesy.cn/#/?url=https://www.baidu.com/?name=React';
     const res = Method.getUrlParam('name', url)
     console.log(res,'00')
+
+    // 解决键盘弹起后遮挡输入框的问题
+    window.addEventListener("resize", () => {
+      if (document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
+        window.setTimeout(function () {
+            document.activeElement.scrollIntoViewIfNeeded();
+        }, 0);
+      }
+    })
   }
 
   render() {
