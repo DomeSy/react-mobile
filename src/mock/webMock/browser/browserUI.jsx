cb477d5fbffc3728b305e6649b8ac167a32a3b73
Domesy <397163123@qq.com>
2021-02-22 11:04:57 +0800

    浏览器是如何渲染UI的

---
+++ src/mock/webMock/browser/browserUI.jsx
@@ -0,0 +1,23 @@
+import img from '@images/browser/webkit.png'
+
+const Index = [
+  {
+    title: '',
+    content: [
+      '浏览器获取HTML⽂件，然后对⽂件进⾏解析，形成DOM Tree',
+      '与此同时，进⾏CSS解析，⽣成Style Rules',
+      '接着将DOM Tree与Style Rules合成为 Render Tree',
+      '接着进⼊布局（Layout）阶段，也就是为每个节点分配⼀个应出现在屏幕上的确切坐标',
+      '随后调⽤GPU进⾏绘制（Paint），遍历Render Tree的节点，并将元素呈现出来'
+    ],
+    method: 'content',
+    type: 'listAll'
+  },{
+    name: 'webkit主流程',
+    height: '100px',
+    content: img,
+    method: 'imgs'
+  },
+]
+
+export default Index
--- src/mock/webMock/browser/index.jsx
+++ src/mock/webMock/browser/index.jsx
@@ -1,9 +1,9 @@
 import Browsers from './Browsers'
 import browserFrom from './browserFrom'
-
+import browserUI from './browserUI'
 const browser = [
   { params: { data: 'browser' }, extra:{noTitle: true}, list: Browsers},
   { params: { data: 'browserFrom' }, extra:{noTitle: true}, list: browserFrom},
-
+  { params: { data: 'browserUI' }, extra:{noTitle: true}, list: browserUI},
 ]
 export default browser
