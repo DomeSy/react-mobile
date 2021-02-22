9d843afebc851ff49c25521959cc06d456ada23e
Domesy <397163123@qq.com>
2021-02-22 10:52:19 +0800

    浏览器的主要组成部分

--- src/mock/webMock/browser/index.jsx
+++ src/mock/webMock/browser/index.jsx
@@ -1,7 +1,9 @@
 import Browsers from './Browsers'
+import browserFrom from './browserFrom'
 
 const browser = [
   { params: { data: 'browser' }, extra:{noTitle: true}, list: Browsers},
+  { params: { data: 'browserFrom' }, extra:{noTitle: true}, list: browserFrom},
 
 ]
 export default browser
