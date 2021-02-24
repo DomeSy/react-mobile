24afc7352f30baaef256af39f853179a5d33561c
Domesy <397163123@qq.com>
2021-02-24 21:50:41 +0800

    代码修改

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,6 @@ class Home extends Component {
 
   componentDidMount = () => {
 
-    // const url = 'http://www.domesy.cn/?name=React';
-    // const url = 'http://www.domesy.cn/#/?name=React';
-    const url = 'http://www.domesy.cn/#/?url=https://www.baidu.com/?name=React';
-    const res = Method.getUrlParam('name', url)
-
     // 解决键盘弹起后遮挡输入框的问题
     window.addEventListener("resize", () => {
       if (document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
