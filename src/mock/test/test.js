4a86f56993801048063f2299dfddddd1a9cd433a
Domesy <397163123@qq.com>
2021-02-25 20:29:50 +0800

    测试

--- src/mock/test/test.js
+++ src/mock/test/test.js
@@ -57,16 +57,3 @@ Mock.mock(/\goods\/goodsList/,"get",function(options){
     return data.data.slice((page-1)*limit,(limit*page)-1)  //data.data是上面的数据，这里获取的是
 })
  
- 
-// export default Mock.mock('/postdata1','post',{
-//     success: true,
-//     message: '@cparagraph',
-//     // 属性 list 的值是一个数组，其中含有 1 到 5 个元素
-//     'list|1-5': [{
-//         // 属性 sid 是一个自增数，起始值为 1，每次增 1
-//         'sid|+1': 1,
-//         // 属性 userId 是一个5位的随机码
-//         'userId|5': '',
-//     }]
-// })
-// eslint-disable
