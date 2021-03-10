549ee7d03cb3545b0e42ba224fe5b8bebd7956f2
Domesy <397163123@qq.com>
2021-03-10 20:52:39 +0800

    跨域场景

--- src/mock/weelMock/cros/Karea.jsx
+++ src/mock/weelMock/cros/Karea.jsx
@@ -1,43 +1,38 @@
 const Index = [
   {
     title: '',
-    content: '前端存储的方式有五种，分别是cookies、localstorage、sessionstorage、Web SQL、IndexedDB',
+    content: '当协议、子域名、主域名、端口号中任意一个不相同时，都算作不同域。不同域之间相互请求资源，就算作“跨域”。',
     method: 'content',
     type: 'blue'
   },{
-    title: 'cookies',
+    title: '',
     content: [
-      '在HTML5标准前本地储存的主要⽅式，优点是兼容性好，请求头⾃带cookie⽅便',
-      '缺点是⼤⼩只有4k， ⾃动请求头加⼊cookie浪费流量，每个domain限制20个cookie，使⽤起来麻烦需要⾃⾏封装'
+      '同一域名下   => 允许',
+      '同一域名下，不同文件   => 允许',
+      '同一域名下，不同端口  => 不允许',
+      '同一域名下，不同协议  => 不允许',
+      '域名和域名对应ip  => 不允许',
+      '主域相同，子域不同  => 不允许',
+      '同一域名，不同二级域名  => 不允许',
+      '不同域名  => 不允许',
     ],
     method: 'content',
     type: 'listAll'
   },{
-    title: 'localstorage(本地存储)',
+    title: '特别说明',
     content: [
-      'HTML5加⼊的以键值对(Key-Value)为标准的⽅式，优点是操作⽅便，永久性储存（除⾮⼿动删 除），⼤⼩为5M，兼容IE8+',
+      '如果是协议和端口造成的跨域问题“前端”是无能为力的。',
+      '在跨域问题上，仅仅是通过“URL的首部”来识别而不会根据域名对应的IP地址是否相同来判断。“URL的首部”可以理解为“协议, 域名和端口必须匹配”。',
     ],
     method: 'content',
     type: 'listAll'
   },{
-    title: 'sessionstorage(会话存储)',
+    title: '请求跨域了，那么请求到底发出去没有？',
     content: [
-      '与localStorage基本类似，区别是sessionStorage当⻚⾯关闭后会被清理，⽽且与cookie、 localStorage不同，他不能在所有同源窗⼝中共享，是会话级别的储存⽅式'
-    ],
-    method: 'content',
-    type: 'listAll'
-  },{
-    title: 'Web SQL',
-    content: [
-      '2010年被W3C废弃的本地数据库数据存储⽅案，但是主流浏览器（⽕狐除外）都已经有了相关的实现',
-      'web sql类似于SQLite，是真正意义上的关系型数据库，⽤sql进⾏操作，当我们⽤JavaScript时要进⾏转换， 较为繁琐。',
-    ],
-    method: 'content',
-    type: 'listAll'
-  },{
-    title: 'IndexedDB',
-    content: [
-      ' 是被正式纳⼊HTML5标准的数据库储存⽅案，它是NoSQL数据库，⽤键值对进⾏储存，可以进⾏快 速读取操作，⾮常适合web场景，同时⽤JavaScript进⾏操作会⾮常⽅便。'
+      '跨域并不是请求发不出去，请求能发出去，服务端能收到请求并正常返回结果，只是结果被浏览器拦截了。',
+      '你可能会疑问明明通过表单的方式可以发起跨域请求，为什么 Ajax 就不会?',
+      '因为归根结底，跨域是为了阻止用户读取到另一个域名下的内容，Ajax 可以获取响应，浏览器认为这不安全，所以拦截了响应。',
+      '但是表单并不会获取新的内容，所以可以发起跨域请求。同时也说明了跨域并不能完全阻止 CSRF，因为请求毕竟是发出去了。',
     ],
     method: 'content',
     type: 'listAll'
