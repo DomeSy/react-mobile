809d089e69a28a441a7bbcad88dae550b7a6ddfb
Domesy <397163123@qq.com>
2021-03-20 09:15:24 +0800

    跳转封装

--- src/unilts/jump/index.jsx
+++ src/unilts/jump/index.jsx
@@ -4,6 +4,7 @@ import { Control } from 'react-keeper';
  * @module 跳转逻辑封装（并获取参数）
  * 
  * @param go 跳转，记录地址栏，一个参数可传字符串(默认跳转首页),没有参数可以直接跳转
+ * @param cache 页面跳转（缓存当前页面，即跳转回来的页面有缓存）
  * @param back 回退(默认回退1)
  * @param replace 重定向（与go一样）
  * @param get 获取地址栏参数
@@ -12,6 +13,10 @@ import { Control } from 'react-keeper';
  * 
  * @param url 跳转的地址
  * @param params 跳转时所带的参数
+ * 
+ * @cache
+ * @param ele 节点内容(可为页面节点，但所包含的组件不能有a标签)
+ * @param params 这里的params必须为对象
  */
 
 class Jump{
@@ -28,6 +33,10 @@ class Jump{
 
   static back = (back = -1) => {
     Control.go(Number(back))
+  }
+
+  static cache = (ele ,url='/', params = {}) => {
+    return <CacheLink to={url} state={params}>{ele}</CacheLink>
   }
 
   static replace = (url='/', params = {}) => {
